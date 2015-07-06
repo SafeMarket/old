1dc71786a60f2e7c898b0a55889d659fc1661c0c
Aakil Fernandes <aakilfernandes@gmail.com>
2015-07-06 14:30:59 -0400

    build 0.0.7

--- bin/build.sh
+++ bin/build.sh
@@ -1,12 +1,18 @@
+if [[ $# -eq 0 ]] ; then
+    echo 'missing tag'
+    exit 0
+fi
+
 cd $(dirname $0)/../
 git checkout master
 git pull
 bower install
-tag="$(git describe --abbrev=0 --tags)"
+tag="$1"
 sed "s/___VERSION___/$tag/g" package.template.json > package.json
 cfx xpi --output-file="builds/safemarket-$tag.xpi"
 sed "s/___VERSION___/$tag/g" readme.template.md > readme.md
 git add -A
 git commit -m "build $tag"
-git push --tags
+git tag "$tag"
 git push
+git push --tags
