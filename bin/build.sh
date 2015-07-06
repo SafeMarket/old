2527d95fe51b6799a825d5c7a779f4a372e7e459
Aakil Fernandes <aakilfernandes@gmail.com>
2015-07-06 12:25:52 -0400

    build 0.0.2

--- bin/build.sh
+++ bin/build.sh
@@ -4,8 +4,10 @@ git pull
 bower install
 tag="$(git describe --abbrev=0 --tags)"
 sed "s/___VERSION___/$tag/g" package.template.json > package.json
-sed "s/___VERSION___/$tag/g" readme.template.md > readme.md
 cfx xpi
+sleep 10
+sed "s/___VERSION___/$tag/g" readme.template.md > readme.md
 git add -A
 git commit -m "build $tag"
 git push
+git push --tags
