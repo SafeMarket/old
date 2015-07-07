e9d240dbb9850f0aa0a7ce9282cc83dc849b1858
Aakil Fernandes <aakilfernandes@gmail.com>
2015-07-07 15:54:37 -0400

    xprv randomizer

--- data/app/SettingsController.js
+++ data/app/SettingsController.js
@@ -27,4 +27,32 @@ app.controller('SettingsController',function($scope,storage,$timeout,ticker,grow
 		growl.addSuccessMessage('Settings saved')
 	}
 
+	$scope.setRandomXprvkey = function(){
+		var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+			,randomString0 = _.sample(characters, 64).join('')
+			,randomString1 = _.sample(characters, 64).join('')
+			,hasher = new jsSHA(randomString0, 'TEXT')
+        	,I = hasher.getHMAC(randomString1, "TEXT", "SHA-512", "HEX")
+       		,il = Crypto.util.hexToBytes(I.slice(0, 64))
+        	,ir = Crypto.util.hexToBytes(I.slice(64, 128))
+			,gen_bip32 = new BIP32();
+        
+        gen_bip32.eckey = new Bitcoin.ECKey(il);
+        gen_bip32.eckey.pub = gen_bip32.eckey.getPubPoint();
+        gen_bip32.eckey.setCompressed(true);
+        gen_bip32.eckey.pubKeyHash = Bitcoin.Util.sha256ripe160(gen_bip32.eckey.pub.getEncoded(true));
+        gen_bip32.has_private_key = true;
+
+        gen_bip32.chain_code = ir;
+        gen_bip32.child_index = 0;
+        gen_bip32.parent_fingerprint = Bitcoin.Util.hexToBytes("00000000");
+        gen_bip32.version = 76066276;
+        gen_bip32.depth = 0;
+
+        gen_bip32.build_extended_public_key();
+        gen_bip32.build_extended_private_key();
+        
+        $scope.settings.xprvkey = gen_bip32.extended_private_key_string()
+	}
+
 })
